0042c39bf18dee359aff3e4e8e975b4a86d8622f
Danni <dannielleorlando@gmail.com>
2017-08-12 13:12:26 -0400

    unit-conversion successful

---
+++ ai/index.js
@@ -0,0 +1,29 @@
+/*
+* Cloud Function
+*/
+'use strict';
+
+const convert = require('convert-units');
+
+exports.momo = (req, res) => {
+  let {fromNumber, fromName, toName} = req.body.result.parameters;
+  //change names to acceptable input for convert function
+  [fromName, toName] = [fromName, toName].map(name => {
+    if (name === 'tbsp'|| name === 'fl oz'){
+      return name = name === 'tbsp' ? 'Tbs' : 'fl-oz'
+    }
+    else return name
+  });
+  const toNumber = convert(fromNumber).from(fromName).to(toName);
+  const output = `${fromNumber} ${fromName} is ${toNumber} ${toName}`;
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ 'speech': output, 'displayText': output }))
+    .catch((error) => {
+  // If there is an error let the user know
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
+  });
+}
+
+//has to use Tbs
