602f4730f9d08e627f87f73354348c06092ad72c
jcblake14 <jcblake14@gmail.com>
2017-08-14 13:22:44 -0400

    Update index.js

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,6 +2,13 @@ const User = require('./user');
 const Recipe = require('./recipe');
 const RecipeBox = require('./recipe_box');
 
+/*
+JB:
+This is where we should use a many-to-many association to associate users with recipes through the Recipe Box.
+By declaring Recipe.belongsToMany(User, {through: RecipeBox}) and User.belongsToMany(Recipe, {through: RecipeBox}), this will
+tell Sequelize to use RecipeBox as the association table between the two models.
+*/
+
 // Recipe.belongsTo(RecipeBox)
 RecipeBox.belongsTo(Recipe);
 RecipeBox.belongsTo(User);
