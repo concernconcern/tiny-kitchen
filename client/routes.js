b00dedf72c9f6629b171524357b5994010942316
Kaitlin Moreno <kaitmore@gmail.com>
2017-08-15 17:32:40 -0400

    finished add recipe

--- client/routes.js
+++ client/routes.js
@@ -3,12 +3,9 @@ import { connect } from 'react-redux'
 import { Router } from 'react-router'
 import { Route, Switch } from 'react-router-dom'
 import PropTypes from 'prop-types'
-// import history from './history'
+import history from './history'
 import { Main, Login, Signup, UserHome, ViewRecipe, CookingAI, AddRecipe } from './components'
 import { me } from './store'
-import createBrowserHistory from 'history/createBrowserHistory';
-let history = createBrowserHistory();
-
 
 /**`
  * COMPONENT
