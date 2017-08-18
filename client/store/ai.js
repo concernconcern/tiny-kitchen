9bc0ad9ea2d10ba6894c35f2c47ada122db12ff7
Sarah Charles <sarah.charles007@gmail.com>
2017-08-18 14:13:38 -0400

    Update ai.js

--- client/store/ai.js
+++ client/store/ai.js
@@ -54,7 +54,6 @@ export const fetchOutput = (userInput) =>
     axios(makeRequestConfig(userInput))
     .then(res => {
       const output = res.data.result.fulfillment.displayText
-      // console.log(output)
       dispatch(getOutput(output));
       dispatch(getOutput(''))
       if (res.data.result.action === 'setTimer'){
