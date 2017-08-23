de8b66e991f3a770d20f0bc51b1e1f773dcbb01c
Sarah Charles <sarah.charles007@gmail.com>
2017-08-23 10:44:26 -0400

    Update AddRecipe.js

--- client/components/AddRecipe.js
+++ client/components/AddRecipe.js
@@ -7,7 +7,7 @@ import * as action from '../store'
 import history from '../history'
 import { GridList, GridTile } from 'material-ui/GridList'
 import { Login } from './auth-form'
-// import ImgUpload from './ImgUpload';
+import ImgUpload from './ImgUpload';
 
 class AddRecipe extends React.Component {
   constructor(props) {
@@ -23,7 +23,6 @@ class AddRecipe extends React.Component {
     this.handleClose = this.handleChange.bind(this);
   }
   componentDidMount() {
-    console.log('PROPS', this.props);
     let recipeUrl = this.props.location.search.slice(5);
     this.props.chromeRecipe(recipeUrl);
   }
